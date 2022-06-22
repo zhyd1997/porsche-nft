3cfdb7888eacf8269ec55afcd69bf268a3dc0ca9
Yadong Zhang <zhyd007@gmail.com>
2022-06-22 06:53:15 +0000

    added prettier into lint staged command.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 "use strict";
 
 module.exports = {
-  "extends": ["next", "prettier", "plugin:storybook/recommended"]
+  extends: ["next", "prettier", "plugin:storybook/recommended"],
 };
--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,12 @@
-const path = require('path');
+const path = require("path");
 
-const buildEslintCommand = filenames => `next lint --fix --file ${filenames.map(f => path.relative(process.cwd(), f)).join(' --file ')}`;
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" --file ")}`;
+const buildPrettierCommand = (filenames) =>
+  filenames.map((filename) => `prettier --write '${filename}'`);
 
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  "*.{js,jsx,ts,tsx}": [buildEslintCommand, buildPrettierCommand],
 };
