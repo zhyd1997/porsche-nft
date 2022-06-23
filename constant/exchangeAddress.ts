5bf2b04c4fe9d4468f5e9d124577033848a5ecb9
Yadong Zhang <zhyd007@gmail.com>
2022-06-23 06:25:04 +0000

    chore: added optional envs.

--- constant/exchangeAddress.ts
+++ constant/exchangeAddress.ts
@@ -1,16 +1,22 @@
 export const exchangeAddress = {
   // stream-in-token
   input: {
-    id: '0xcaa7349cea390f89641fe306d93591f87595dc1f',
-    symbol: 'USDCx',
+    id:
+      process.env.NEXT_PUBLIC_TOKEN_A_ADDRESS?.toLowerCase() ||
+      "0xcaa7349cea390f89641fe306d93591f87595dc1f",
+    symbol: process.env.NEXT_PUBLIC_TOKEN_A_SYMBOL || "USDCx",
   },
   // contract address
   exchange: {
-    id: '0x86c2b55bf5d3e9dac2747389b38d41c6b1f34179',
+    id:
+      process.env.NEXT_PUBLIC_EXCHANGE_ADDRESS?.toLowerCase() ||
+      "0x86c2b55bf5d3e9dac2747389b38d41c6b1f34179",
   },
   // distribute-out-token
   output: {
-    id: '0x263026e7e53dbfdce5ae55ade22493f828922965',
-    symbol: 'RIC',
+    id:
+      process.env.NEXT_PUBLIC_TOKEN_B_ADDRESS?.toLowerCase() ||
+      "0x263026e7e53dbfdce5ae55ade22493f828922965",
+    symbol: process.env.NEXT_PUBLIC_TOKEN_B_SYMBOL || "RIC",
   },
 };
